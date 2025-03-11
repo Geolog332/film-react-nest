18a7b3f7154a0540e982e7d6436ed78ef5a0b59d
Anton Mataev <Geolog332@yahoo.com>
2025-03-12 02:01:27 +0500

    fix: review

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -7,16 +7,41 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DATABASE_HOST'),
-        port: configService.get('DATABASE_PORT'),
-        username: configService.get('DATABASE_USERNAME'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
-        entities: [__dirname + '/../*/**/*.entity{.js, .ts}'],
-        synchronize: false,
-      }),
+      useFactory: (configService: ConfigService) => {
+        const databaseType = configService.get<'postgres' | 'mongodb'>(
+          'DATABASE_DRIVER',
+        );
+
+        // Общая конфигурация
+        const commonConfig = {
+          host: configService.get('DATABASE_HOST'),
+          port: configService.get('DATABASE_PORT'),
+          username: configService.get('DATABASE_USERNAME'),
+          password: configService.get('DATABASE_PASSWORD'),
+          database: configService.get('DATABASE_NAME'),
+          entities: [__dirname + '/../*/**/*.entity{.js, .ts}'],
+          synchronize: false,
+        };
+
+        // Конфигурация для PostgreSQL
+        if (databaseType === 'postgres') {
+          return {
+            type: 'postgres',
+            ...commonConfig,
+          };
+        }
+
+        // Конфигурация для MongoDB
+        if (databaseType === 'mongodb') {
+          return {
+            type: 'mongodb',
+            url: configService.get('DATABASE_URL'),
+            ...commonConfig,
+          };
+        }
+
+        throw new Error(`Unsupported database type: ${databaseType}`);
+      },
       inject: [ConfigService],
     }),
   ],
